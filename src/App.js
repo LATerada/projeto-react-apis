008645a03257ac37327f9b191d6b044b51191d14
LATerada <larissaakiterada@gmail.com>
2023-02-17 13:40:48 -0300

    Exporting all the files js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import { createGlobalStyle } from "styled-components"
+import { PokemonListPage } from "./Pages/PokemonsListPage/PokemonListPage";
+
+const GlobaStyle = createGlobalStyle`
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+`
+
 function App() {
   return (
     <div>
-
+      <GlobaStyle/>
+      <PokemonListPage/>
     </div>
   );
-}
+};
 
 export default App;
