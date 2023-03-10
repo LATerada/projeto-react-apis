1819220bc2b45c0722792ab1f71db5db8cd0d43f
LATerada <larissaakiterada@gmail.com>
2023-03-10 17:58:03 -0300

    Bug fix

--- src/Hooks/useRequesData.js
+++ src/Hooks/useRequesData.js
@@ -3,10 +3,10 @@ import { useEffect, useState } from "react"
 import { BASE_URL } from "../constants/url"
 
 export const useRequestData = (initialState, path) => {
-    const [data, setData] = useState(initialState)
-    const [isLoading, setIsLoading] = useState(false)
-    const [isLoaded, setIsLoaded] = useState(false)
-    const [error,setError] = useState(false)
+    const [ data, setData ] = useState(initialState)
+    const [ isLoading, setIsLoading ] = useState(false)
+    const [ isLoaded, setIsLoaded ] = useState(false)
+    const [ error,setError ] = useState(false)
 
     useEffect(() => {
         setIsLoading(true)
